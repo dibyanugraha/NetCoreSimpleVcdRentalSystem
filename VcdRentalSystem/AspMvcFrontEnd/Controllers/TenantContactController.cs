987318eef205c77350a225290543338558cff613
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-01-11 16:51:04 +0700

    Try to add new view for report

---
+++ VcdRentalSystem/AspMvcFrontEnd/Controllers/TenantContactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspMvcFrontEnd.Models;
+
+namespace AspMvcFrontEnd.Controllers
+{
+    public class TenantContactController : Controller
+    {
+        List<TenantModel> tenants = new List<TenantModel>();
+        List<ContactModel> contacts = new List<ContactModel>();
+
+        public IActionResult Index()
+        {
+            var joinedTable = from tenant in tenants
+                                   join contact in contacts on tenant.TenantId equals contact.TenantId into newTable
+                                   from contact in newTable.DefaultIfEmpty()
+                                   select new TenantContactModel { Contact = contact, Tenant = tenant };
+            return View(joinedTable);
+        }
+    }
+}
